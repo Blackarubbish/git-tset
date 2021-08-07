365a203459c68d13845f1af439dfe0f0efb1485e
Blackarubbish <81726319+Blackarubbish@users.noreply.github.com>
2021-08-07 21:04:19 +0800

    try to commit

--- src/main/java/gitTest.java
+++ src/main/java/gitTest.java
@@ -3,7 +3,6 @@ public class gitTest {
     public static void main(String[] arg){
         System.out.println("git1");
         System.out.println("git2");
-        System.out.println("git3");
         System.out.println("git4");
         System.out.println("git1");
 
